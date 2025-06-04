cc22c6cbf291190e3b484d462fc7c88c52fd5ce1
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2025-06-04 17:12:40 +0700

    fix: remove duplicate entry from PUBLIC_GET_URLS and adjust PUBLIC_URLS

--- src/main/java/com/spring/app/constants/WhitelistUrlConstant.java
+++ src/main/java/com/spring/app/constants/WhitelistUrlConstant.java
@@ -2,15 +2,16 @@ package com.spring.app.constants;
 
 public class WhitelistUrlConstant {
   public static final String[] PUBLIC_URLS = {
+      "/auth/**"
+  };
+
+  public static final String[] PUBLIC_GET_URLS = {
       "/v3/api-docs/**",
       "/swagger-ui/**",
       "/swagger-ui.html",
       "/swagger-resources/**",
       "/swagger-ui/**",
       "/swagger-ui.html",
-      "/auth/**"
   };
-
-  public static final String[] PUBLIC_GET_URLS = {};
 
 }
